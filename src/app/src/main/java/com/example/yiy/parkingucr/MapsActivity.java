9703d043fbeb9369d69767e9144bda716c8c8e89
YhzyY <huangzi_yi96111@users.noreply.github.com>
2018-11-30 11:37:22 -0800

    add the location of 6 lots on the map

--- src/app/src/main/java/com/example/yiy/parkingucr/MapsActivity.java
+++ src/app/src/main/java/com/example/yiy/parkingucr/MapsActivity.java
@@ -39,8 +39,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Loc_Lot32 = new LatLng(33.970106, -117.33114);
+        mMap.addMarker(new MarkerOptions().position(Loc_Lot32).title("Lot32"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_Lot32));
+
+        LatLng Loc_Lot30 = new LatLng(33.969962, -117.33186);
+        mMap.addMarker(new MarkerOptions().position(Loc_Lot30).title("Lot30"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_Lot30));
+
+        LatLng Loc_Lot26 = new LatLng(33.981603, -117.334914);
+        mMap.addMarker(new MarkerOptions().position(Loc_Lot26).title("Lot26"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_Lot26));
+
+        LatLng Loc_Lot24 = new LatLng(33.978089, -117.330567);
+        mMap.addMarker(new MarkerOptions().position(Loc_Lot24).title("Lot24"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_Lot24));
+
+        LatLng Loc_Lot6 = new LatLng(33.969771, -117.327454);
+        mMap.addMarker(new MarkerOptions().position(Loc_Lot6).title("Lot6"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_Lot6));
+
+        LatLng Loc_BigSprings = new LatLng(33.975179, -117.320979);
+        mMap.addMarker(new MarkerOptions().position(Loc_BigSprings).title("Big Springs Road Parking Lot"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_BigSprings));
     }
 }
