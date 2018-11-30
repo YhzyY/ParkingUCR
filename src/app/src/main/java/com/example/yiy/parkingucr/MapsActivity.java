a8db90f053fa75f46c37aef5c711991fb946139d
YhzyY <huangzi_yi96111@users.noreply.github.com>
2018-11-30 14:39:55 -0800

    get the current location

--- src/app/src/main/java/com/example/yiy/parkingucr/MapsActivity.java
+++ src/app/src/main/java/com/example/yiy/parkingucr/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.yiy.parkingucr;
-
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +17,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
     private GoogleMap mMap;
+    LocationManager locationManager;
+    public static String Current_Long;
+    public static String Current_Lat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +31,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        CheckPermission();
     }
 
+    /* Request updates at startup */
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLocation();
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void CheckPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // Getting reference to TextView tv_longitude
+
+        System.out.println("这里是onLocationChanged : " + location);
+        // Setting Current Longitude
+        Current_Long = String.valueOf(location.getLongitude());
+        // Setting Current Latitude
+        Current_Lat = String.valueOf(location.getLatitude());
+
+        System.out.println("这里是具体结果：longtitude : " + Current_Long + "latitude : " + Current_Lat);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider!" + provider,
+                Toast.LENGTH_SHORT).show();
+       }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
+    }
 
     /**
      * Manipulates the map once available.
@@ -62,5 +128,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng Loc_BigSprings = new LatLng(33.975179, -117.320979);
         mMap.addMarker(new MarkerOptions().position(Loc_BigSprings).title("Big Springs Road Parking Lot"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_BigSprings));
+//
+//        LatLng Loc_Current = new LatLng(currlat, currlon);
+//        mMap.addMarker(new MarkerOptions().position(Loc_Current).title("This is you current address"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc_Current));
+
+
+
     }
+
 }
